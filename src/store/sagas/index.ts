b630d4db4f8ec9440ecd4202bf2a50f8e68c44e2
weizheng <weizheng@kaikeba.com>
2021-06-22 19:45:31 +0800

    wip: reduce and saga

--- src/store/sagas/index.ts
+++ src/store/sagas/index.ts
@@ -1,23 +1,16 @@
 import { all } from 'redux-saga/effects';
-import login from './modules/login';
+// import login from './modules/login';
 
-// const modules = import.meta.globEager('./modules/**/*.ts');
+const modules = import.meta.globEager('./modules/**/*.ts');
 
-// const sagaModules: any = {};
+const sagaModules: any = [];
 
-// Object.keys(modules).forEach((key) => {
-//   console.log(key);
-//   const mod = modules[key].default || {};
-//   const objKey = key && key.match(/modules\/(\S*).ts/);
-//   if (objKey && objKey.length > 0) {
-//     sagaModules[objKey[1]] = mod;
-//   }
-// });
-// const sagas = { ...sagaModules };
-
-// export default function* rootSaga(): any {
-//   return yield all([sagas]);
-// }
+Object.keys(modules).forEach((key) => {
+  console.log(key);
+  const mod = modules[key].default || {};
+  sagaModules.push(mod);
+});
+const sagas = [...sagaModules];
 export default function* rootSaga(): any {
-  return yield all([login]);
+  return yield all([...sagas]);
 }
