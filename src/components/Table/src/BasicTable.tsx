ded1961712bf0a36bd3b681c168b5e1698acde68
weizheng <weizheng@kaikeba.com>
2021-06-29 17:17:26 +0800

    wip: table comp useDatasource func

--- src/components/Table/src/BasicTable.tsx
+++ src/components/Table/src/BasicTable.tsx
@@ -1,43 +1,44 @@
+/*
+ * @Author: weizheng
+ * @Date: 2021-06-22 20:30:54
+ * @LastEditors: weizheng
+ * @LastEditTime: 2021-06-29 17:09:57
+ */
 import React, { useState } from 'react';
 import { Table, Space } from 'antd';
-import { BasicTableProps } from './types/table';
+import { BasicTableProps, ActionItem } from './types/table';
 // import TableColumn from './compontent/TableColumn';
 // import TableAction from './compontent/TableAction';
 import { ColumnProps, ColumnType, TablePaginationConfig } from 'antd/es/table';
-import { FilterValue, SorterResult } from 'antd/es/table/interface';
+import { SpinProps } from 'antd/es/spin';
 
-import { isFunction } from '/@/utils/is';
+import { useImmer } from 'use-immer';
+
+import { useDataSource } from './hooks/useDataSource';
 
 const { Column, ColumnGroup } = Table;
 
 const BasicTable: React.FC<BasicTableProps> = (props) => {
-  const { actions, columns, tableProps, sortFn, filterFn } = props;
-  const [pagination, setPagination] = useState<false | TablePaginationConfig>(
+  const { actions, columns, tableProps } = props;
+  const [loading, setLoading] = useState<boolean | SpinProps | undefined>(props.tableProps.loading);
+  const [pagination, setPagination] = useImmer<false | TablePaginationConfig>(
     tableProps.pagination || false
   );
 
-  const handleChange = (
-    pagination: TablePaginationConfig,
-    filters: Record<string, FilterValue | null>,
-    sorter: SorterResult<Recordable> | SorterResult<Recordable>[]
-  ) => {
-    setPagination(pagination);
-    const params: Recordable = {};
-    if (sorter && isFunction(sortFn)) {
-      // const sortInfo = sortFn(sorter);
-      // searchState.sortInfo = sortInfo;
-      // params.sortInfo = sortInfo;
-    }
+  const { handleTableChange, dataSource } = useDataSource(props, {
+    paginationInfo: pagination,
+    setPagination,
+    setLoading,
+  });
 
-    if (filters && isFunction(filterFn)) {
-      // const filterInfo = filterFn(filters);
-      // searchState.filterInfo = filterInfo;
-      // params.filterInfo = filterInfo;
-    }
-    fetch(params);
-  };
   return (
-    <Table {...tableProps} pagination={pagination} onChange={handleChange}>
+    <Table
+      {...tableProps}
+      pagination={pagination}
+      onChange={handleTableChange}
+      loading={loading}
+      dataSource={dataSource}
+    >
       {columns?.map((item: ColumnProps<Recordable>, index: number) => {
         if (item.children) {
           const list: ColumnType<Recordable>[] = item.children;
@@ -57,13 +58,11 @@ const BasicTable: React.FC<BasicTableProps> = (props) => {
         key="action"
         render={(...params) => (
           <Space size="middle">
-            {actions.map((item) => {
-              return (
-                <a key={item.label} onClick={() => item.onClick && item.onClick(...params)}>
-                  Invite {item.label}
-                </a>
-              );
-            })}
+            {actions.map((item: ActionItem) => (
+              <a key={item.label} onClick={() => item.onClick && item.onClick(...params)}>
+                Invite {item.label}
+              </a>
+            ))}
           </Space>
         )}
       />
---
+++ src/components/Table/src/hooks/useDataSource.ts
@@ -0,0 +1,144 @@
+/*
+ * @Author: weizheng
+ * @Date: 2021-06-29 16:02:24
+ * @LastEditors: weizheng
+ * @LastEditTime: 2021-06-29 17:09:32
+ */
+
+import { BasicTableProps, FetchParams } from '../types/table';
+import { FilterValue, SorterResult } from 'antd/es/table/interface';
+import { TablePaginationConfig } from 'antd/es/table';
+import { useImmer, Updater } from 'use-immer';
+
+import { get } from 'lodash-es';
+import { isFunction, isBoolean } from '/@/utils/is';
+import { FETCH_SETTING, PAGE_SIZE } from '../const';
+
+interface ActionType {
+  paginationInfo: TablePaginationConfig | boolean;
+  setPagination: Updater<TablePaginationConfig>;
+  setLoading: (loading: boolean) => void;
+}
+
+interface SearchState {
+  sortInfo: Recordable;
+  filterInfo: Record<string, string[]>;
+}
+export function useDataSource(
+  props: BasicTableProps,
+  { paginationInfo, setPagination, setLoading }: ActionType
+) {
+  const { sortFn, filterFn } = props;
+  const [searchState, setSeatchState] = useImmer<SearchState>({
+    sortInfo: {},
+    filterInfo: {},
+  });
+  const [dataSource, setDataSource] = useImmer<Recordable[]>([]);
+
+  const handleTableChange = (
+    pagination: TablePaginationConfig,
+    filters: Record<string, FilterValue | null>,
+    sorter: SorterResult<Recordable> | SorterResult<Recordable>[]
+  ) => {
+    setPagination(pagination);
+    const params: Recordable = {};
+    if (sorter && isFunction(sortFn)) {
+      const sortInfo = sortFn(sorter);
+      setSeatchState((draft) => {
+        draft.sortInfo = sortInfo;
+      });
+      params.sortInfo = sortInfo;
+    }
+
+    if (filters && isFunction(filterFn)) {
+      const filterInfo = filterFn(filters);
+      setSeatchState((draft) => {
+        draft.filterInfo = filterInfo;
+      });
+      params.filterInfo = filterInfo;
+    }
+    fetch(params);
+  };
+  async function fetch(opt?: FetchParams) {
+    const { api, searchInfo, fetchSetting, beforeFetch, afterFetch, tableProps } = props;
+    const { pagination } = tableProps;
+    if (!api || !isFunction(api)) return;
+    try {
+      setLoading(true);
+      const { pageField, sizeField, listField, totalField } = Object.assign(
+        FETCH_SETTING,
+        fetchSetting
+      );
+      let pageParams: Recordable = {};
+
+      const { current = 1, pageSize = PAGE_SIZE } = paginationInfo as TablePaginationConfig;
+
+      if ((isBoolean(pagination) && !pagination) || isBoolean(paginationInfo)) {
+        pageParams = {};
+      } else {
+        pageParams[pageField] = (opt && opt.page) || current;
+        pageParams[sizeField] = pageSize;
+      }
+
+      const { sortInfo = {}, filterInfo } = searchState;
+
+      let params: Recordable = {
+        ...pageParams,
+        ...searchInfo,
+        ...(opt?.searchInfo ?? {}),
+        ...sortInfo,
+        ...filterInfo,
+        ...(opt?.sortInfo ?? {}),
+        ...(opt?.filterInfo ?? {}),
+      };
+      if (beforeFetch && isFunction(beforeFetch)) {
+        params = beforeFetch(params) || params;
+      }
+
+      const res = await api(params);
+
+      const isArrayResult = Array.isArray(res);
+
+      let resultItems: Recordable[] = isArrayResult ? res : get(res, listField);
+      const resultTotal: number = isArrayResult ? 0 : get(res, totalField);
+
+      // 假如数据变少，导致总页数变少并小于当前选中页码，通过getPaginationRef获取到的页码是不正确的，需获取正确的页码再次执行
+      if (resultTotal) {
+        const currentTotalPage = Math.ceil(resultTotal / pageSize);
+        if (current > currentTotalPage) {
+          setPagination((draft) => {
+            draft.current = currentTotalPage;
+          });
+          fetch(opt);
+        }
+      }
+
+      if (afterFetch && isFunction(afterFetch)) {
+        resultItems = afterFetch(resultItems) || resultItems;
+      }
+      setDataSource(() => {
+        return resultItems;
+      });
+      setPagination({
+        total: resultTotal || 0,
+      });
+      if (opt && opt.page) {
+        setPagination({
+          current: opt.page || 1,
+        });
+      }
+    } catch (error) {
+      setDataSource([]);
+      setPagination({
+        total: 0,
+      });
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  return {
+    dataSource,
+    handleTableChange,
+  };
+}
