9aedc45d860c2c7d9c17a3737a9d378f8d8591a7
weizheng <weizheng@kaikeba.com>
2021-08-28 23:53:07 +0800

    wip: test hook1

--- src/layouts/header/index.tsx
+++ src/layouts/header/index.tsx
@@ -50,7 +50,7 @@ const LayoutHeader: React.FC<HeaderProps> = ({ collapsed, toggle }) => {
               </Menu>
             }
           >
-            <span className="drop-title">姓名333</span>
+            <span className="drop-title">姓名4444</span>
           </Dropdown>
         </div>
       </div>
