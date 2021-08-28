2246341bd528f737b37dd0956231d832a3261d11
weizheng <weizheng@kaikeba.com>
2021-08-28 20:19:16 +0800

    wip: docker test

--- src/layouts/header/index.tsx
+++ src/layouts/header/index.tsx
@@ -50,7 +50,7 @@ const LayoutHeader: React.FC<HeaderProps> = ({ collapsed, toggle }) => {
               </Menu>
             }
           >
-            <span className="drop-title">姓名</span>
+            <span className="drop-title">姓名1</span>
           </Dropdown>
         </div>
       </div>
